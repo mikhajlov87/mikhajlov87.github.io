458bb481b4437891e8b49aa77269c69ff8d7be8e
Oksana Mazun <misssevenka@gmail.com>
2017-06-30 13:38:06 +0300

    Add profile layout

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import TopBar from '../components/TopBar';
+import { Link } from 'react-router-dom';
+import Icon from 'react-fontawesome';
 
 class Profile extends Component {
   render() {
     return (
-      <div>
-        <TopBar/>
+      <div className="profile container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+            <Link to="/" className="profile__close-profile close-profile btn">
+              <span className="close-profile__text">Закрыть кабинет &nbsp;</span>
+              <Icon name="sign-out"></Icon>
+            </Link>
+          </div>
+          <div className="profile__info col-md-6"></div>
+        </div>
       </div>
     );
   }
